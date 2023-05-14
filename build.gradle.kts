e6155789a423ceaee71f6cc13806b7929a6dd26d
Alexei Yashkov <alexei.yashkov@yashkov.org>
2023-05-14 11:34:23 -0400

    Fix the root project name, disable jvmToolchain for now

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,9 +39,9 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.3")
 }
 
-kotlin {
-    jvmToolchain(11)
-}
+//kotlin {
+//    jvmToolchain(11)
+//}
 
 tasks {
     test {
