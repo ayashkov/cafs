1547c3736a668039b3085de323b16ddededc754c
Alexei Yashkov <alexei.yashkov@yashkov.org>
2023-05-14 10:46:19 -0400

    Temporarily disable publishing and private repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,37 +1,37 @@
 plugins {
     kotlin("jvm") version "1.8.21"
-    id("maven-publish")
-    id("net.linguica.maven-settings") version "0.5"
+//    id("maven-publish")
+//    id("net.linguica.maven-settings") version "0.5"
 }
 
 group = "org.yashkov.cafs"
 
 repositories {
     mavenCentral()
-    maven {
-        name = "yashkov"
-        url = uri("https://repo.dev.yashkov.org/repository/maven-yashkov/")
-    }
+//    maven {
+//        name = "yashkov"
+//        url = uri("https://repo.dev.yashkov.org/repository/maven-yashkov/")
+//    }
 }
 
-publishing {
-    repositories {
-        maven {
-            name = "yashkov"
-            url = uri(
-                if (version.toString().endsWith("-SNAPSHOT"))
-                    "https://repo.dev.yashkov.org/repository/maven-snapshots/"
-                else
-                    "https://repo.dev.yashkov.org/repository/maven-releases/"
-            )
-        }
-    }
-    publications {
-        create<MavenPublication>("java") {
-            from(components["java"])
-        }
-    }
-}
+//publishing {
+//    repositories {
+//        maven {
+//            name = "yashkov"
+//            url = uri(
+//                if (version.toString().endsWith("-SNAPSHOT"))
+//                    "https://repo.dev.yashkov.org/repository/maven-snapshots/"
+//                else
+//                    "https://repo.dev.yashkov.org/repository/maven-releases/"
+//            )
+//        }
+//    }
+//    publications {
+//        create<MavenPublication>("java") {
+//            from(components["java"])
+//        }
+//    }
+//}
 
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.3")
